46c01b293c7ed11c66a0cb656689a19544e19d67
791616 <43044160+791616@users.noreply.github.com>
2018-10-23 09:15:01 -0700

    first commit

---
+++ New Boston/Calculator/Simple calculator/apples.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class apples here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class apples{
+public static void main (string args[]) {
+    Scanner jet = new Scanner (System.in);
+    double fnum, snum, answer
+    
+    
+    
+    
+    
+}
+}
