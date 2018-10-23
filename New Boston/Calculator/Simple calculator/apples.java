a92c240bf6d45c4d715e3d9db4afa6e6a26dc5d4
Tristan Schnur <tschnur@WS-ACADBUS.EDU>
2018-10-23 10:18:44 -0700

    r

--- New Boston/Calculator/Simple calculator/apples.java
+++ New Boston/Calculator/Simple calculator/apples.java
@@ -7,15 +7,27 @@
  */
 import java.util.Scanner;
 public class apples{
-public static void main (String args[]) {
-    Scanner jet = new Scanner (System.in);
-    double fnum, snum, answer;
-    System.out.println("Enter first num: ");
-    fnum=jet.nextDouble();
-    System.out.println("Enter second num: ");
-    snum=jet.nextDouble();
-    answer = fnum + snum;
-    System.out.println(answer);
     
-}
+    
+    public static void main (String args[]) {
+        
+        
+        Scanner jet = new Scanner (System.in);
+        double fnum, snum, answer;
+        String Opp = "";
+        System.out.println("Choose one: add, sub, mult, div ");
+        Opp=jet.nextLine();
+        System.out.println("Opp= "+Opp);
+        System.out.println("Enter first num: ");
+        fnum=jet.nextDouble();
+        System.out.println("Enter second num: ");
+        snum=jet.nextDouble();
+        answer ="fnum + snum"; 
+        answer ="fnum - snum";
+        answer ="fnum / snum";
+        answer ="fnum * snum";
+        System.out.println(answer);
+
+    }
+    
 }
